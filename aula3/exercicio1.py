b8ce9f6b07220665de9d93bc7f8bd269243a3472
José Pereira Macedo <arseniumgx@gmail.com>
2021-06-10 19:37:46 -0300

    Estrutura if...else melhorada

--- aula3/exercicio1.py
+++ aula3/exercicio1.py
@@ -8,9 +8,10 @@ Faça um programa que pergunte ao usuário um número e valide se o numero é pa
 
 numero = int(input("Digite um número: "))
 
-if numero == 0:
-    print("Número é zero!")
-elif numero % 2 == 0:
-    print(f'{numero} é par!')
+if numero != 0:
+    if numero % 2 == 0:
+        print(f'{numero} é par!')
+    else:
+        print(f'{numero} é impar!')
 else:
-    print(f'{numero} é impar!')
+    print("Número é zero!")
