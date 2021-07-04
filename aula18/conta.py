0a9dc470be2e226af4bff09f2aa778200c6c1c18
José Pereira Macedo <arseniumgx@gmail.com>
2021-07-04 14:49:40 -0300

    Aula 18 - orientacao a objetos; mesmo codigo ate o fim do modulo

--- aula18/conta.py
+++ aula18/conta.py
@@ -1,30 +1,50 @@
 class Conta:
-    def __init__(self, titular, saldo = 0):
-        self.titular = titular
-        self.__saldo = saldo
+    def __init__(self):
+        self.__titular = str()
+        self.__saldo = float()
 
     def __str__(self):
         if self.__saldo == 0:
             return f'''
-            Titular: {self.titular}
+            Titular: {self.__titular}
             '''
         else:
             return f'''
-            Titutar: {self.titular}
+            Titutar: {self.__titular}
             Saldo : {self.__saldo}
             '''
 
     def depositar(self, valor):
         self.__saldo += valor
         return f'''
-        {self.titular} seu saldo atual é de {self.__saldo}
+        {self.__titular} seu saldo atual é de {self.__saldo}
         '''
 
+    def __autorizaSaque(self, valor):
+        return self.__saldo > 0 and valor <= self.__saldo
+
     def sacar(self, valor):
-        if self.__saldo > 0 and valor <= self.__saldo:
+        if self.__autorizaSaque(valor):
             self.__saldo -= valor
             return f'''
-            {self.titular} seu saldo atual é de {self.__saldo}
+            {self.__titular} seu saldo atual é de {self.__saldo}
             '''
         else:
             return 'Saldo insuficiente.'
+
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @saldo.setter
+    def saldo(self, valor):
+        self.__saldo = valor
+    
+    
+    @property
+    def titular(self):
+        return self.__titular
+
+    @titular.setter
+    def titular(self, name):
+        self.__titular = name
--- aula18/main.py
+++ aula18/main.py
@@ -1,13 +1,45 @@
-from .conta import Conta
+from conta import Conta
+
+conta001 = Conta()
+conta001.titular = 'José'
+conta001.saldo = 300
+
+print(conta001.titular)
+print(conta001.saldo)
+
+conta001.sacar(100)
+print(conta001.saldo)
 
 
-if __name__ == "__main__":
-    banco = list()
 
-    for i in range(2):
-        conta = Conta(input('Titular: '))
-        banco.appen(conta)
 
-    banco[0].depositar(500)
-    banco[1].depositar()
-    print(banco)
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # banco = list()
+
+    # for i in range(2):
+    #     conta = Conta(input('Titular: '))
+    #     banco.appen(conta)
+
+    # banco[0].depositar(500)
+    # banco[1].depositar()
+    # print(banco)
